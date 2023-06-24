6502f40c72c01e2e3709b1f185aa5a06648b369c
JohnXArbiter <1439972069@qq.com>
2023-06-24 22:12:43 +0800

    增加优雅程度

---
+++ go-go-zero/service/file/cmd/api/internal/middleware/jwtauthMiddleware.go
@@ -0,0 +1,17 @@
+package middleware
+
+import (
+	"go-go-zero/common/middleware"
+	"net/http"
+)
+
+type JwtAuthMiddleware struct {
+}
+
+func NewJwtAuthMiddleware() *JwtAuthMiddleware {
+	return &JwtAuthMiddleware{}
+}
+
+func (m *JwtAuthMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
+	return middleware.JwtAuthentication(next)
+}
